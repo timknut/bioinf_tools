29890b4c09765812e43f91d274b0c027f2a2526d
Tim Knutsen <timknutsen@gmail.com>
2014-08-08 10:42:13 +0200

    Add [n threasds] argument

--- SlurmTemp.py
+++ SlurmTemp.py
@@ -1,15 +1,16 @@
 #!/bin/env python
 
 # Sends your unix command in a slurm script to Cluster
-# usage: SlurmTemp.py "unix_command"
+# usage: SlurmTemp.py "unix_command" [threads]
 
 import os
 import sys
 
 command = sys.argv[1]
+threads = sys.argv[2] ## Use som version of sprintf to manipulate the #SBATCH -n string.
 	
 with open("slurm_template", "w") as template:
-    template.write("#!/bin/bash -x\n#SBATCH -J Leech\n#SBATCH -N 1\n#SBATCH -n 1\n" + str(command) + "\n")
+    template.write("#!/bin/bash -x\n#SBATCH -J SlurmTemp\n#SBATCH -N 1\n#SBATCH -n " + threads + "\n" + str(command) + "\n")
 
 os.system("sbatch slurm_template")
 os.remove("slurm_template")
