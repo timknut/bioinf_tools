0c44c4934d481ef07be1461055a053a4ee85b19d
Tim Knutsen <timknut@users.noreply.github.com>
2015-02-27 15:38:57 +0100

    On the way to being generic,
     taking a file or object with chrom and pos and snp as input.

--- get_flanking_seq_Bovine.R
+++ get_flanking_seq_Bovine.R
@@ -1,18 +1,26 @@
-# Load the BSgenome package and download the Btau6 reference sequence 
-# For documentation see http://www.bioconductor.org/packages/release/bioc/html/BSgenome.html
 source("http://bioconductor.org/biocLite.R")
-biocLite("BSgenome")
-biocLite("BSgenome.Btaurus.UCSC.bosTau6") #installs the cow genome (~750 MB download).
-library('BSgenome.Btaurus.UCSC.bosTau6')
+if (!require(BSgenome.Btaurus.UCSC.bosTau6)) {
+	message("Downloading genome")
+	biocLite("BSgenome.Btaurus.UCSC.bosTau6")
+}
 
 # Function to pull flanking sequence. Defaults to +/- 10 bp
-getflank <- function(position, alleles="[N/N]", chr="chrN", offset=10) {
-   leftflank  <- getSeq(Btaurus,chr,position-offset,position-1)
-   rightflank <- getSeq(Btaurus,chr,position+1,position+offset)
-   paste(leftflank,alleles,rightflank,sep="")
+getflank <- function(position, alleles="[N/N]", chr="chrN", offset=100) {
+	leftflank  <- getSeq(Btaurus,chr,position-offset,position-1)
+	rightflank <- getSeq(Btaurus,chr,position+1,position+offset)
+	paste(leftflank,alleles,rightflank,sep="")
+}
+
+
+## Make function so script can take list of snps and positions from vcf like file and give flanks.
+parse_input <- function(flanklist) {
+	if (is.character(flanklist)) {
+		df <- read.table(flanklist, stringsAsFactors = FALSE)
+		
+	}
 }
 
 # Create an example from dbSNP flank from UMD 3.1
- 
+
 #   http://www.ncbi.nlm.nih.gov/sites/entrez?db=snp&cmd=search&term=rs1520218
-getflank(54254, alleles="[C/A]")
+getflank(54254, alleles="[C/A]", chr = "chr2", offset=100)
