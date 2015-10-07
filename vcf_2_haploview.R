ecad092c5fd7cd3db0edfa62a2e91c571e33f5cb
Tim Knutsen <timknutsen@gmail.com>
2015-10-07 15:36:09 +0200

    vcf_2_haploview.R works and LS is calulated concistently between vcftools and Haploview woth Haps-format

--- vcf_2_haploview.R
+++ vcf_2_haploview.R
@@ -4,6 +4,7 @@
 ##	ID1	ID2
 ##	0|1	1|1
 ##	1|1	0|0
+
 ## To:
 
 ## ID1	A	T
@@ -13,20 +14,16 @@
 
 # Set lib.path for use on Cigene cluster.
 .libPaths( c( .libPaths(), "/mnt/users/tikn/R/x86_64-redhat-linux-gnu-library/3.2") )
-
 library(argparser, quietly=TRUE)
 # Create a parser
-p <- arg_parser("Convert phased VCF file to Haplovew Haps format")
-
+p <- arg_parser(
+"Convert VCF file to Haplovew Haps format. Detects if file is gzipped.
+Appends .dat and locusinfo.txt to output files.")
 # Add command line arguments
-p <- add_argument(p, "VCF", help="Phased VCF file to convert", type="character")
-#p <- add_argument(p, "--digits", help="number of decimal places", default=0)
-
+p <- add_argument(p, "VCF", help="Phased VCF file to convert.", type="character")
 # Parse the command line arguments
 argv <- parse_args(p)
-
-# Do work based on the passed arguments
-# cat( round(argv$number, argv$digits), "\n")
+vcf <- argv$VCF
 
 load_packages <- function(){
 require(data.table, quietly = T)
@@ -36,16 +33,15 @@ library(splitstackshape, quietly = T)
 suppressPackageStartupMessages(library(dplyr, quietly = T))
 }
 
-load_packages() ;  cat("Loading a perverse number of packages for this trivial task\n")
-#vcf <- "~/Projects/Fatty_acids_bovine/variant_calling/example_LD_haploview_10_snps_beagle.vcf"
-vcf <- argv$VCF
-#vcf <- "example_LD_haploview_10_snps.vcf"
-
-
+load_packages() ;  cat("\nLoading a perverse number of packages for this trivial task\n\n")
+#vcf <- "example_LD_haploview_10_snps_beagle.vcf.gz"
+#vcf <- "FA_sign_variants_allelic_primitives_Q30.with_ID_exclude_3454_1893_GQ30.snps.noprim_beaglephased.vcf.gz"
 geno2alleles <- "~tikn/bioinf_tools/freebayes/vcflib/bin/vcfgeno2alleles"
-## Skip ## in VCF and convert 0 1 to ACGT.
-command <- sprintf("%s < %s | grep -v '##'", geno2alleles, vcf)
-dict <- c("A" = 1, "C" = 2, "G" = 3, "T" = 4, "." = 0)
+dict <- c("A" = 1, "C" = 2, "G" = 3, "T" = 4, "." = 0) # haploview alleles.
+
+gzipped <- ifelse(str_detect(vcf, "vcf$"), "cat", "zcat")
+## Skip ## in VCF text or zipped and convert 0/1 genotypes to ACGT.
+command <- sprintf("%s  %s | %s | grep -v '##'", gzipped, vcf, geno2alleles)
 
 # function for writing datafiles
 write_data <- function(df,filename) {
@@ -58,19 +54,21 @@ infile <- fread(command, header = T, sep = "\t")
 #system(sprintf("cat %s", vcf))
 #setnames(infile, "#CHROM", "CHROM")
 samples <- str_trim(names(infile))
-sample_col_start <- which(str_detect(samples, "FORMAT")) + 2
+sample_col_start <- which(str_detect(samples, "FORMAT")) + 1
 
-outfile <- ddply(infile, .(POS), str_extract, "[ACGT]/[ACGT]")
-samplecols <- seq(sample_col_start, ncol(outfile), 1)
-outfile <- select(outfile, samplecols) %>% tbl_dt()
-
+outfile <- apply(infile, 2, str_extract, "[ACGT]/[ACGT]")
+outfile <- data.frame(outfile[,10:ncol(outfile)])
 setnames(outfile, names(outfile), samples[10:length(samples)])
+
 outfile <- cSplit(outfile, names(outfile), "/")
 outfile <- apply(outfile, 2, function(x) dict[x]) %>% t() # mysteriously change ACGT to 1234 and transpose
 outfile <- cbind("Fam", str_replace(rownames(outfile), pattern = "_1|_2", ""), outfile)
 
 ## Write data file
-write_data(outfile, filename = sprintf("%s.dat", vcf))
+outname <- str_replace(vcf, "\\.vcf$|\\.vcf\\.gz$", "")
+
+write_data(outfile, filename = sprintf("%s.dat", outname))
 ## Write locus info file
 write_data(df = cbind(paste("marker_", 1:nrow(infile), sep = ""), select(infile,POS)),
-			  filename = sprintf("%s.locusinfo.txt", vcf))
+			  filename = sprintf("%s.locusinfo.txt", outname))
+	cat(sprintf("Convertion finished.\nOutput files are:\n%s.dat\n%s.locusinfo.txt\n\n", outname, outname))
